439ff91a1a78c05b4f06d078eaac6681280132f6
AdamGonda <adamgondagyula@gmail.com>
2019-10-27 06:58:27 +0100

    using groups

--- main.py
+++ main.py
@@ -1,36 +1,15 @@
 import re
 
-def get_range(n1, n2):
-    return list(range(min(int(n1), int(n2)), max(int(n1), int(n2)) + 1))
-
-def calc_floors(item):
-    if item.find('--') > 0:
-        n1 = re.match(r'(\A-?\d*)', item).group() 
-        rest = item[len(n1):]
-        n2 = rest[1:]
-        return get_range(n1, n2)
-    
-    else:
-        n1 = re.match(r'(\A-?\d*)', item).group()
-        rest = item[len(n1):]
-        if len(rest) > 0:
-            n2 = rest[1:]
-            return get_range(n1, n2)
-
-        return int(n1)
-
 def main(floors):
     result = []
     for item in floors.split(','):
-        r = calc_floors(item.strip())
-        try:
-            result.extend(r)
-        except:
-            result.append(r)
-
+        matches = re.match(r'(-?\d+)-?(-?\d+)?', item.strip())
+        n1 = matches.group(1)
+        n2 = matches.group(2)
+        result.extend(list(range(int(n1), int(n2 or n1) + 1)))
     return sorted(set(result))
 
 
 if __name__=='__main__':
-    floors = '-16, 1, 1-7, -4-6, -1--4, 4--7'
+    floors = '-1, 2, -7--1'
     print(main(floors))
